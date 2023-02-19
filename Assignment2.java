a5f96fdc5f164177f749c59256e1fcd39a8f1acd
Ege Ertem <ege.ertem@ug.bilkent.edu.tr>
2023-02-19 18:57:38 +0300

    Display Menu Added

--- Assignment2.java
+++ Assignment2.java
@@ -1,14 +1,12 @@
-/**
- * Assignment2
- */
+import java.util.Random;
+
 public class Assignment2 {
-<<<<<<< HEAD
     private Integer[] ArrayOfNumbers;
     private int randomNumber;
     private Random rd;
     private Assignment2(int ArraySize)
     {
-        ArrayOfNumbers = new String[ArraySize];
+        ArrayOfNumbers = new Integer[ArraySize];
         rd = new Random();
         for(int i = 0; i<ArraySize; i++)
         {
@@ -16,5 +14,8 @@ public class Assignment2 {
             ArrayOfNumbers[i] = randomNumber;
         }
     }
-    
+    public static void displayMenu()
+    {
+        System.out.println("1- Find the min\n2- Find the max\n3- Find the average\n4- Find the sum of odd index\n5- Find the sum of even index\n6- Exit\nWhat is your choice?:\n");
+    }   
 }
