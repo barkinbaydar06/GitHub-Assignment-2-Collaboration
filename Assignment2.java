9f7ab1484792ea6af06696a8ea260b33adc5cffc
barkinbaydar <barkin.baydar@ug.bilkent.edu.tr>
2023-02-19 19:50:38 +0300

    Final code

--- Assignment2.java
+++ Assignment2.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Assignment2 {
     private Integer[] ArrayOfNumbers;
@@ -16,7 +18,7 @@ public class Assignment2 {
     }
     public static void displayMenu()
     {
-        System.out.println("1- Find the min\n2- Find the max\n3- Find the average\n4- Find the sum of odd index\n5- Find the sum of even index\n6- Exit\nWhat is your choice?:\n");
+        System.out.print("1- Find the min\n2- Find the max\n3- Find the average\n4- Find the sum of odd index\n5- Find the sum of even index\n6- Exit\nWhat is your choice?: ");
     }
     public int minNum()
     {
@@ -84,7 +86,60 @@ public class Assignment2 {
         for (int i = 0; i < ArrayOfNumbers.length; i++) {
             sum += ArrayOfNumbers[i];
         }
-        int average = sum/ArrayOfNumbers.length;
+         int average = sum/ArrayOfNumbers.length;
         return average;
+    }
+    public void printArray()
+    {
+        for(int i = 0; i<ArrayOfNumbers.length; i++)
+        {
+            System.out.print(ArrayOfNumbers[i]+ " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int size;
+        boolean quit = false;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        size = in.nextInt();
+        Assignment2 array = new Assignment2(size);
+        array.printArray();
+        do 
+        {
+        displayMenu();
+        int choice = in.nextInt();
+        if (choice == 1) 
+        {
+            int minNum = array.minNum();
+            System.out.println("Minimum number in this array is: "+minNum);    
+        }    
+        if (choice == 2) 
+        {
+            int maxNum = array.maxNum();
+            System.out.println("Maximum number in this array is: " + maxNum);    
+        }
+        if (choice == 3) 
+        {
+               int average = array.findAverage();
+               Integer[] difference = array.findDifferenceAverage();
+               System.out.println("Average of the array is: " + average);
+               System.out.println("Differences of numbers in the array from the average are: "+ Arrays.toString(difference) ); 
+        }
+        if (choice == 4) 
+        {
+               int sumOfOddIndexes = array.sumOfOddIndex();
+               System.out.println("Sum of the numbers with odd indexes in the array is: "+ sumOfOddIndexes);
+        }
+        if (choice == 5) 
+        {
+            int sumOfEvenIndex = array.getSumOfEvenIndexes();
+            System.out.println("Sum of the numbers with even indexes in the array is: " + sumOfEvenIndex);
+        }
+        if (choice == 6) 
+        {
+            quit = true;    
+        }
+        } while (!quit);
     }
 }
