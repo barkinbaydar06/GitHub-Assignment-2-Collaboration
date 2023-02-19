b9cd4a0864bd882daa3c49d9c10f2141fd160c38
barkinbaydar <barkin.baydar@ug.bilkent.edu.tr>
2023-02-19 18:49:48 +0300

    Constructer Added

--- Assignment2.java
+++ Assignment2.java
@@ -2,6 +2,18 @@
  * Assignment2
  */
 public class Assignment2 {
-
+    private Integer[] ArrayOfNumbers;
+    private int randomNumber;
+    private Random rd;
+    private Assignment2(int ArraySize)
+    {
+        ArrayOfNumbers = new String[ArraySize];
+        rd = new Random();
+        for(int i = 0; i<ArraySize; i++)
+        {
+            randomNumber = rd.nextInt(0, 100);
+            ArrayOfNumbers[i] = randomNumber;
+        }
+    }
     
 }
